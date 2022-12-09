ee328e9bf03bd094af531633f9f9e3b51584193c
NB-22082506 <NB-22082506@NB-22082506.mshome.net>
2022-12-09 10:32:14 +0900

    branch commit

--- CodingTest/src/test1/ThreeToTen.java
+++ CodingTest/src/test1/ThreeToTen.java
@@ -8,6 +8,7 @@ public class ThreeToTen {
 	//commit한다
 	// web에서 commit한다
 	// push만 한다
+	// branch 연습
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		List<Integer> toThreeReverse = findThree(125);
